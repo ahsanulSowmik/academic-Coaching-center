eeee8ff62ac8b1f7d3a77a77a11c45d29f4ec90d
MD.Ahsanul Islam <MD.Ahsanul Islam@DESKTOP-2KAO3CE>
2020-12-13 03:14:02 +0600

    this is a mini project by java

---
+++ src/Code/student.java
@@ -0,0 +1,29 @@
+
+package Code;
+public class student {
+    private String name;
+    private String userName;
+    private String id;
+    private String email;
+    private String mobile;
+    private String result;
+    private String password;
+    
+    
+    public void login() {
+        
+    }
+    public void registration() {
+        
+    }
+    public void viewProfile() {
+        
+    }
+    public void selectCourses() {
+        
+    }
+     public void viewCourseplan() {
+        
+    }
+    
+}
